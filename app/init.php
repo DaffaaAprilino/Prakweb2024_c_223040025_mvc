a523ac0002c09c62d082f9f200b83699c7cb4399
Daffaa Aprilino <daffaaprilino7@gmail.com>
2024-10-20 16:33:55 +0700

    video 8 database Wrapper

--- app/init.php
+++ app/init.php
@@ -1,6 +1,12 @@
-<?php 
+<?php
 
+// inisasi
+
+// Core adalah classnya
 require_once 'core/App.php';
+// controler ini utama, controllers extend
 require_once 'core/Controller.php';
-
-require_once 'core/Constants.php';
+// dbnya
+require_once 'core/Database.php';
+// Konstanta
+require_once 'config/config.php';
